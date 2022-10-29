c8222f987794b89446e624de83813832436653ac
DK <darshankhot@programmer.net>
2022-10-29 16:53:26 +0530

    added filters song cards

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,9 +1,35 @@
 import "./Filters.scss";
 
+import searchIcon from "../../assets/search.svg";
+
+// components
+import SongCard from "./SongCard/SongCard";
+
 function Filters() {
   return (
     <aside className="Filters">
-      Filters Section
+      <header className="header">
+        <h2>
+          For You
+        </h2>
+      </header>
+
+      <form className="search-form">
+        <label className="search-box">
+          <input type="text" placeholder="Search Song, Artist" />
+          <button type="submit">
+            <img src={searchIcon} alt="Search" />
+          </button>
+        </label>
+      </form>
+
+      <ul className="songs-list">
+        <SongCard />
+        <SongCard />
+        <SongCard />
+        <SongCard selected={true} />
+        <SongCard />
+      </ul>
     </aside>
   )
 };
